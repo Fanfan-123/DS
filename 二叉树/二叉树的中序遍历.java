97ed4ad643d4e9188d462a80614823bdd9e1f71f
Fanfan-123 <49780497+Fanfan-123@users.noreply.github.com>
2019-04-27 19:52:10 +0800

    二叉树的中序遍历
    思想：
      （1）递归遍历
      （2）中序遍历的顺序：左子树，根结点，右子树

---
+++ 二叉树/二叉树的中序遍历.java
@@ -0,0 +1,18 @@
+public class Main {
+    private static class Node{
+        char val;
+        Node left;
+        Node right;
+
+        Node(char v){
+            this.val = v;
+        }
+    }
+    private static void inOrderTraversal(Node root){
+        if(root != null){
+            inOrderTraversal(root.left);
+            System.out.print(root.val+"");
+            inOrderTraversal(root.right);
+        }
+    }
+  }
