e24886ece411f1f9a0217032453ee1bf495ef71b
Fanfan-123 <49780497+Fanfan-123@users.noreply.github.com>
2019-05-08 20:59:55 +0800

    栈的基本操作.java
    1，压栈
    2，出栈
    3，查看栈顶元素
    4，查看栈中元素个数

--- 栈/栈的基本操作.java
+++ 栈/栈的基本操作.java
@@ -1,1 +1,27 @@
+public class MyStack {
+    private int[] array;
+    int top;
+    MyStack(){
+        this.array = new int[100];
+        this.top = 0;
+    }
 
+
+    //压栈
+    public void push(int v){
+        this.array[this.top++] = v;
+    }
+    //出栈
+    public int pop(){
+        return this.array[--this.top];
+    }
+    //查看栈顶元素
+    public int peek(){
+        return this.array[top-1];
+    }
+    //查看栈中元素大小
+    public int size(){
+        return this.top;
+    }
+
+}
