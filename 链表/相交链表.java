04eed6db6be98ae150a207cf280a183963b3c1f2
Fanfan-123 <49780497+Fanfan-123@users.noreply.github.com>
2019-08-15 22:56:33 +0800

    相交链表.java

---
+++ 链表/相交链表.java
@@ -0,0 +1,31 @@
+class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode last = headB;
+        while (last.next != null) {
+            last = last.next;
+        }
+        last.next = headB;
+
+        ListNode fast = headA;
+        ListNode slow = headA;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                slow = headA;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                last.next = null;
+                return fast;
+            }
+        }
+        last.next = null;
+        return null;
+    }
+}
