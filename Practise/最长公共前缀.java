3f772a48a78dbbbb8990c8d0911e25bc18f17422
Fanfan-123 <49780497+Fanfan-123@users.noreply.github.com>
2019-08-15 23:05:07 +0800

    最长公共前缀.java

---
+++ Practise/最长公共前缀.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+     if (strs.length == 1){
+            return strs[0];
+        }
+        StringBuilder sb = new StringBuilder();
+        if (strs.length>1) {
+            int len = strs[0].length();
+            for (int i = 0; i < len; i++) {
+                char curr = strs[0].charAt(i);
+                for (int j = 1; j < strs.length; j++) {
+                    if (strs[j].length()<=i ||strs[j].charAt(i) != curr) {
+                        return sb.toString();
+                    }
+                    if (strs[j].charAt(i) == curr && j == strs.length - 1) {
+                        sb.append(curr);
+                    }
+                }
+            }
+        }
+       return sb.toString();
+    }
+}
