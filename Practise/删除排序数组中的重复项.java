3416cfdd6d4478ccbb7b6050164b9e05f40ab8ed
Fanfan-123 <49780497+Fanfan-123@users.noreply.github.com>
2019-08-17 22:55:12 +0800

    删除排序数组中的重复项.java

---
+++ Practise/删除排序数组中的重复项.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        // 使用双指针
+        if(nums==null || nums.length == 1){
+            return nums.length;
+        }
+        int i = 0,j =1;
+        while(j<nums.length){
+            if(nums[i]==nums[j]){
+                j++;
+            }else{
+                i++;
+                nums[i]=nums[j];
+                j++;
+            }
+        }
+        return i+1;
+    }
+}
