651c65dca99476f145092f42fe7ea23558778be9
Fanfan-123 <49780497+Fanfan-123@users.noreply.github.com>
2019-08-19 16:44:40 +0800

    二叉树的前序遍历2.java

---
+++ 二叉树/二叉树的前序遍历2.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+     List<Integer> list = new ArrayList<>();
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if(root != null){
+            list.add(root.val);
+            preorderTraversal(root.left);
+            preorderTraversal(root.right);
+        }
+        return list;
+    }
+}
