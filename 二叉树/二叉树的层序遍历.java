b01bbc0fff5474b17acc74d255dfc5471a34fac2
Fanfan-123 <49780497+Fanfan-123@users.noreply.github.com>
2019-08-20 21:31:57 +0800

    二叉树的层序遍历.java

---
+++ 二叉树/二叉树的层序遍历.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> listReturn = new ArrayList<>();
+        if(root == null){
+            return listReturn;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int count = queue.size();
+            List<Integer> list = new ArrayList<>();
+            while(count > 0){
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if(node.left != null){
+                    queue.add(node.left);
+                }
+                if(node.right != null){
+                    queue.add(node.right);
+                }
+                count--;
+            }
+            listReturn.add(list);
+        }
+        return listReturn;
+    }
+}
